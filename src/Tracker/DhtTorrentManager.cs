5f78043d6a3e4277925d41cd60d2562ee86b9ff8
Jiangyan Xu <jiangyan@ufl.edu>
2007-11-15 17:37:44 -0500

    Copied the whole tracker codebase from bitsharp due to the trouble to extend it (Singletons, etc). DhtTracker is moved to Tracker.cs as an option to startup the tracker

--- src/Tracker/DhtTorrentManager.cs
+++ src/Tracker/DhtTorrentManager.cs
@@ -2,200 +2,56 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using MonoTorrent.Common;
-using MonoTorrent.Tracker;
 using Ipop;
-using System.Diagnostics;
-using MonoTorrent.BEncoding;
 
 namespace FuseSolution.Tracker {
-  class DhtTorrentManager : MonoTorrent.Tracker.ITorrentManager {
-    #region Fields
-    private Torrent torrent;
-    private IDht _dht;
-    private PeerManager peers;
-    private int complete = 0;
-    private int downloaded;
-    #endregion
-
-    #region Properties
-    /**
-     * identifies the torrent which we are managing
-     */
-    public Torrent Torrent {
-      get {
-        return torrent;
-      }
-    }
-
-    /**
-     * Counts the peers downloading/uploading this torrent.
-     */
-    public int Count {
-      get {
-        return peers.Count;
-      }
-    }
-
-    /**
-     * Number of peers with the entire file
-     */
-    public int CountComplete {
-      get {
-        return complete;
-      }
-    }
-
-    /**
-     * Total number of times the tracker has registered a completion
-     */
-    public int Downloaded {
-      get {
-        return downloaded;
-      }
-    }
-
-    #endregion
-
-    #region Constructors
-    public DhtTorrentManager(Torrent torrent) {
-      this.torrent = torrent;
-      peers = new PeerManager();
-    }
+  class DhtTorrentManager : ITorrentManager {
+    IDht _dht;
 
     public DhtTorrentManager(Torrent torrent, IDht dht) {
-      this.torrent = torrent;
-      this._dht = dht;
-    } 
-    #endregion
-
-    #region Public Methods
-
-    public void Add(MonoTorrent.Tracker.AnnounceParameters par) {
-      string key = Peer.GetKey(par);
-      Debug.WriteLine("adding peer: " + par.ip + ":" + par.port);
-
-      if (peers.Contains(key)) {
-        Debug.WriteLine("peer already in there. maybe the client restarted?");
-        peers.Remove(key);
-      }
-
-      Peer peer = new Peer(par, new System.Threading.TimerCallback(PeerTimeout));
-
-      peers.Add(peer);
-
-      if (peer.IsCompleted) {
-        System.Threading.Interlocked.Increment(ref complete);
-        System.Threading.Interlocked.Increment(ref downloaded);
-      }
+      _dht = dht;
     }
 
-    /**
-     * removes the peer with par.ip and par.port
-     */
-    public void Remove(MonoTorrent.Tracker.AnnounceParameters par) {
-      string key = Peer.GetKey(par);
-      Debug.WriteLine("removing: |" + key + "|");
-      peers.Remove(key);
+
+
+    #region ITorrentManager Members
+
+    public MonoTorrent.Common.Torrent Torrent {
+      get { throw new Exception("The method or operation is not implemented."); }
     }
 
-    /**
-     * this method is used for computing the list of peers which share this torrent
-     * @return if par.compact is true then the value is a BEncodedString otherwise it's a BEncodedDictionary 
-     */
-    public MonoTorrent.BEncoding.BEncodedValue GetPeersList(MonoTorrent.Tracker.AnnounceParameters par) {
-      if (par.compact) {
-        return GetCompactList(par);
-      } else {
-        return GetNonCompactList(par);
-      }
+    public int CountComplete {
+      get { throw new Exception("The method or operation is not implemented."); }
     }
 
-    /**
-     * this method returns the scrape entry for this torrent
-     */
+    public int Downloaded {
+      get { throw new Exception("The method or operation is not implemented."); }
+    }
+
+    public int Count {
+      get { throw new Exception("The method or operation is not implemented."); }
+    }
+
+    public MonoTorrent.BEncoding.BEncodedValue GetPeersList(AnnounceParameters par) {
+      throw new Exception("The method or operation is not implemented.");
+    }
+
     public MonoTorrent.BEncoding.BEncodedDictionary GetScrapeEntry() {
-      BEncodedDictionary dict = new BEncodedDictionary();
-
-      dict.Add("complete", new BEncodedNumber(CountComplete));
-      dict.Add("downloaded", new BEncodedNumber(Downloaded));
-      dict.Add("incomplete", new BEncodedNumber(Count - CountComplete));
-      if (!torrent.Equals(String.Empty))
-        dict.Add("name", new BEncodedString(torrent.Name));
-
-      return dict;
+      throw new Exception("The method or operation is not implemented.");
     }
 
-    /**
-     * update the internale torrent datas and the peer with par.ip and par.port
-     */
-    public void Update(MonoTorrent.Tracker.AnnounceParameters par) {
-      string key = Peer.GetKey(par);
-      Debug.WriteLine("updating peer: " + par.ip + ":" + par.port);
-      if (!peers.Contains(key)) {
-        Add(par);
-        Console.Error.WriteLine("warning: Peer not managed. If you restarted the Tracker ignore this message");
-        return;
-      }
-      Peer peer = peers.Get(key);
-
-      if (par.@event.Equals(TorrentEvent.Completed)) {
-        System.Threading.Interlocked.Increment(ref complete);
-        System.Threading.Interlocked.Increment(ref downloaded);
-      }
-
-      peer.Update(par);
+    public void Add(AnnounceParameters par) {
+      throw new Exception("The method or operation is not implemented.");
     }
 
-    #endregion
-
-    #region Private Methods
-    //TODO refactor - done not debuged
-    private BEncodedValue GetCompactList(AnnounceParameters par) {
-      Peer exclude = null;
-      if (peers.Contains(Peer.GetKey(par))) {
-        exclude = peers.Get(Peer.GetKey(par));
-      }
-      List<Peer> randomPeers = peers.GetRandomPeers(par.numberWanted, exclude);
-      byte[] peersBuffer = new byte[randomPeers.Count * 6];
-      int offset = 0;
-      Debug.WriteLine("Number of peers returned: " + randomPeers.Count);
-      foreach (Peer each in randomPeers) {
-        byte[] entry = each.CompactPeersEntry;
-        Array.Copy(entry, 0, peersBuffer, offset, entry.Length);
-        offset += entry.Length;
-      }
-//    Debug.WriteLine("stream.length: "+stream.Length);
-//    Debug.WriteLine("stream.buffer.length: "+stream.GetBuffer().Length);
-//    Debug.Assert(stream.GetBuffer().Length == stream.Length);
-      return new BEncodedString(peersBuffer);
+    public void Remove(AnnounceParameters par) {
+      throw new Exception("The method or operation is not implemented.");
     }
 
-    //TODO refactor: done - not debuged
-    private BEncodedValue GetNonCompactList(AnnounceParameters par) {
-      Peer exclude = peers.Get(Peer.GetKey(par));
-      List<Peer> randomPeers = peers.GetRandomPeers(par.numberWanted, exclude);
-      BEncodedList announceList = new BEncodedList(randomPeers.Count);
-
-      foreach (Peer each in randomPeers) {
-        announceList.Add(each.PeersEntry);
-      }
-
-      return announceList;
+    public void Update(AnnounceParameters par) {
+      throw new Exception("The method or operation is not implemented.");
     }
 
-    //this is the handle from the peer timer. it is called when the peer is not responding anymore.
-    //if this is the case we remove em to save memory. this is neccesary if peers shut down but do 
-    //not send the finished update. 
-    private void PeerTimeout(object peer) {
-      Debug.WriteLine("peer is not updating anymore");
-      Peer p = peer as Peer;
-
-      if (p == null) {
-        throw new ArgumentException("not a Peer instance", "peer");
-      }
-
-      Remove(p);
-    }
     #endregion
   }
 }
--- src/Tracker/TorrentManagerFactory.cs
+++ src/Tracker/TorrentManagerFactory.cs
@@ -7,11 +7,11 @@ using Ipop;
 using FuseSolution.Common;
 
 namespace FuseSolution.Tracker {
+  public enum TrackerType {
+    Local, Dht, Simple
+  }
+
   class TorrentManagerFactory {
-    
-    public enum TrackerType {
-      Local, Dht, Simple
-    }
 
     public static ITorrentManager GetTorrentManager(TrackerType t, Torrent torrent) {
       switch (t) {
