69da109e33322767c61a774dc46fc198261b9bf1
Jiangyan Xu <jiangyan@ufl.edu>
2009-11-22 21:45:32 -0500

    [Fix] Forgot to create the mountpoint directory before starting FUSE system... [Improvement] Allow server to run on interface assigned by installation script instead of only "tapipop".

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 # This script builds the fushare project and copies binaries to make a package.
 
+print_help_and_exit() {
+  printf "Usage: %s: [-bvc] \n" $(basename $0) >&2; 
+  cat <<EOF
+  -b Build application.
+  -v Verbose.
+  -c Compress and make a tarball.
+EOF
+  exit 2
+}
+
 while getopts "bvc" optNames; do
 case "$optNames" in
   b) build=1;;
   v) verbose=1;;
   c) compress=1;;
+  ?) print_help_and_exit;;
   esac
 done
 
@@ -19,17 +30,22 @@ fushare_src="$fushare_proj/src"
 if [ "$build" ]; then
   echo "Building Fushare..."
   "$fushare_scripts/make_fushare.sh" -b
+  if [ $? -ne 0 ]; then
+    echo "Build failed. Exiting..."
+    exit 1
+  fi
+
+  client_proj_name="FushareApp"
+  web_proj_name="Fushare.Web"
+  client_dir="$fushare_src/$client_proj_name/bin/l4n"
+  web_dir="$fushare_src/$web_proj_name"
+
+  rsync -avLm --delete --include-from="$scripts_dir/rsync.rules" --exclude-from="$scripts_dir/rsync.rules" "$client_dir/" "$proj_dir/client/bin/"
+  rsync -avLm --delete --include-from="$scripts_dir/rsync.rules" --exclude-from="$scripts_dir/rsync.rules" "$web_dir/" "$proj_dir/server/bin/"
 fi
 
-client_proj_name="FushareApp"
-web_proj_name="Fushare.Web"
-client_dir="$fushare_src/$client_proj_name/bin/l4n"
-web_dir="$fushare_src/$web_proj_name"
-
-rsync -avLm --delete --include-from="$scripts_dir/rsync.rules" --exclude-from="$scripts_dir/rsync.rules" "$client_dir/" "$proj_dir/client/bin/"
-rsync -avLm --delete --include-from="$scripts_dir/rsync.rules" --exclude-from="$scripts_dir/rsync.rules" "$web_dir/" "$proj_dir/server/bin/"
-
 if [ "$compress" ]; then
-  tar czvf "$proj_dir/../gatorrent.tar.gz" "$proj_dir" --exclude=".git*"
+  # Make a tarball gatorrent.tar.gz with gatorrent as the root directory inside.
+  tar -C $proj_dir/.. -czvf "$proj_dir/../gatorrent.tar.gz" gatorrent --exclude=".git*" --exclude="*~"
 fi
 
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 # This script runs the gatorrent application.
 
+print_help_and_exit() {
+  printf "Usage: %s: [-scfv] \n" $(basename $0) >&2; 
+  cat <<EOF
+  -s  Run server.
+  -c  Run client.
+  -v  Verbose.
+  -f  Run applications as foreground processes.
+EOF
+  exit 2
+}
+
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
@@ -12,6 +23,7 @@ case "$optNames" in
   c) run_client=1;;
   f) foreground=1;;
   v) verbose=1;;
+  ?) print_help_and_exit;;
   esac
 done
 
@@ -27,32 +39,40 @@ client_bin="$proj_dir/client/bin"
 server_bin="$proj_dir/server/bin"
 mount_point="/mnt/gatorrent"
 
+test ! $foreground && bg_suffix="&"
+
 if [ "$run_server" ]; then
   echo "Running server..."
-  if [ "$foreground" ]; then
-    xsp2 --root "$server_bin"
-  else
-    xsp2 --root "$server_bin" --nonstop &
-  fi
+  eval xsp2 --root "$server_bin" --nonstop $bg_suffix
 fi
 
 if [ "$run_client" ]; then
   if [[ `mount | grep "/dev/fuse"` ]]; then
     umount -vl "/dev/fuse"
   fi
-  mkdir -p "$proj_dir/client/var/shadow"
+
+  mkdir -p $proj_dir/client/var/shadow
+  mkdir -p $mount_point 
+
   echo "Running client..."
-  libdir="../client/bin"
-  if [ "$foreground" ]; then
-    LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}$libdir" \
-    mono --debug "$client_bin/FushareApp.exe" -o allow_other -m "$mount_point" -s "$proj_dir/client/var/shadow"
-  else
-    LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}$libdir" \
-    mono --debug "$client_bin/FushareApp.exe" -o allow_other -m "$mount_point" -s "$proj_dir/client/var/shadow" &
-  fi
+  libdir=$client_bin
+    
+  # Make sure module loaded.
+  modprobe fuse
+  while [[ ! -e /dev/fuse ]]; do
+    sleep 1s
+  done
+
+  eval LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}$libdir" \
+    mono --debug "$client_bin/FushareApp.exe" -o allow_other -m "$mount_point" -s "$proj_dir/client/var/shadow" $bg_suffix
+
+  # Make sure the file system is fully started.
+  while [[ -z `mount | grep "/dev/fuse"` ]]; do
+    echo "FUSE is not up yet. Sleeping 1s..."
+    sleep 1s
+  done
+
   if [ ! "$foreground" ]; then
-    # Make sure the file system is fully started.
-    sleep 2s
     namespace=$(hostname)
     mkdir -p "$mount_point/bittorrent/$namespace"
   fi
