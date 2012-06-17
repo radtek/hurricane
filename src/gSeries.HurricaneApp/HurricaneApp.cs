2a0d464837b3a7d15439b0b7cea796bd6403c419
Xu, Jiang Yan <me@jxu.me>
2012-06-17 03:44:19 -0400

    Add HurricaneApp project which is going to be the entry point for Hurricane.

---
+++ src/gSeries.HurricaneApp/HurricaneApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GSeries.DataDistributionService;
+using Ninject;
+using Ninject.Extensions.Wcf;
+using System.Reflection;
+
+namespace GSeries.HurricaneApp {
+    public class HurricaneApp {
+        public static void Main(string[] args) {
+            var kernel = new StandardKernel();
+            kernel.Load(new ServiceNinjectModule());
+            KernelContainer.Kernel = kernel;
+
+            new HurricaneServiceManager().Start();
+            Console.Read();
+        }
+
+    }
+}
